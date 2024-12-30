fe3491fdde29792e243feb4b5680c83c95797870
thapaliyasudip <connectwithsudip@gmail.com>
2024-12-30 16:29:10 +0545

    Add Models for API Request and Response

---
+++ JobCandidateHub.Model/CandidateModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobCandidateHub.Model
+{
+    public class CandidateModel
+    {
+        [Required(ErrorMessage = "First Name is Required Field.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is Required Field.")]
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email Adress is Required Field.")]
+        [EmailAddress(ErrorMessage = "Please Enter Valid Email Adresses.")]
+        public string Email { get; set; }
+
+        public DateTime? TimeIntervalStart { get; set; }
+
+        public DateTime? TimeIntervalEnd { get; set; }
+
+        [Url]
+        public string LinkedIn { get; set; }
+
+        [Url]
+        public string GitHub { get; set; }
+
+        [MaxLength(10000)]
+        [Required(ErrorMessage = "Comment can't be empty.")]
+        public string Comment { get; set; }
+    }
+}
---
+++ JobCandidateHub.Model/ResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobCandidateHub.Model
+{
+    public class ResponseModel<T>
+    {
+        public T Data { get; set; }
+        public int StatusCode { get; set; }
+        public bool SuccessStatus { get; set; }
+        public string[] ErrorMessage { get; set; }
+    }
+}
